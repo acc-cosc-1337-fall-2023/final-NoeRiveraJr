156ffa091272ec178c333e31d63776bc8b1b09eb
Noe Rivera Jr <noe.rivera.jr@outlook.com>
2023-12-09 22:24:30 +0000

    Question 2 done

--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -1,12 +1,13 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "die.h"
+#include "roll.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
-TEST_CASE("Testing roll function") 
+TEST_CASE("Testing die class functions") 
 {
 	srand((unsigned) time(NULL));
 	Die dice;
@@ -17,8 +18,28 @@ TEST_CASE("Testing roll function")
 	for(int i = 0; i < 10; i++)
 	{
 		dice.roll();
-		std::cout<<dice.rolled_value()<<"\n";
 		REQUIRE(dice.rolled_value() >= 1);
 		REQUIRE(dice.rolled_value() <= 6);
 	}
 }
+
+TEST_CASE("Testing roll class functions") 
+{
+	srand((unsigned) time(NULL));
+	Die dice1;
+	Die dice2;
+	Roll roll(dice1,dice2);
+	roll.roll_die();
+	REQUIRE(roll.roll_value() >= 2);
+	REQUIRE(roll.roll_value() <= 12);
+
+	for(int i = 0; i < 10; i++)
+	{
+		roll.roll_die();
+		std::cout<<roll.roll_value()<<"\n";
+		REQUIRE(roll.roll_value() >= 2);
+		REQUIRE(roll.roll_value() <= 12);
+	}
+}
+
+
