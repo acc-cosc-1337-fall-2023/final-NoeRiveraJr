dcce96f5a4d7eeca4ce7dd5a69d0ed7b0b5a2588
Noe Rivera Jr <noe.rivera.jr@outlook.com>
2023-12-09 22:58:26 +0000

    Question 3 done

--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -2,6 +2,7 @@
 #include "catch.hpp"
 #include "die.h"
 #include "roll.h"
+#include "shooter.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -36,10 +37,28 @@ TEST_CASE("Testing roll class functions")
 	for(int i = 0; i < 10; i++)
 	{
 		roll.roll_die();
-		std::cout<<roll.roll_value()<<"\n";
 		REQUIRE(roll.roll_value() >= 2);
 		REQUIRE(roll.roll_value() <= 12);
 	}
+}
+
+TEST_CASE("Testing shooter class functions")
+{
+	srand((unsigned) time(NULL));
+	Die dice1;
+	Die dice2;
+	Shooter shooter;
+	Roll* roll = shooter.throw_die(dice1,dice2);
+	REQUIRE(roll->roll_value() >= 2);
+	REQUIRE(roll->roll_value() <= 12);
+	for(int i = 0; i < 10; i++)
+	{
+		roll = shooter.throw_die(dice1,dice2);
+		std::cout<<roll->roll_value()<<"\n";
+		REQUIRE(roll->roll_value() >= 2);
+		REQUIRE(roll->roll_value() <= 12);
+	}
+
 }
 
 
